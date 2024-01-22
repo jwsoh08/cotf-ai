5cbcae8d162a2872af4cd60b79123f4994cb4f57
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2024-01-22 16:53:30 +0800

    Fix relative import issue by defining Secrets Class in file

--- lcc/k_mapp.py
+++ lcc/k_mapp.py
@@ -4,7 +4,31 @@ import re
 import openai
 from openai import OpenAI
 
-from ..basecode.services.aws import SecretsManager
+
+# For AWS Secrets
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+
+class SecretsManager:
+    @staticmethod
+    def get_secret(key):
+        secret_name = "cotf/streamlit/test"
+        region_name = "ap-southeast-1"
+
+        # Create a Secrets Manager client
+        session = boto3.session.Session()
+        client = session.client(service_name="secretsmanager", region_name=region_name)
+
+        try:
+            get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+
+        except ClientError as e:
+            raise e
+
+        secrets = json.loads(get_secret_value_response["SecretString"])
+        return secrets[key]
 
 
 def mermaid(code: str) -> None:
