3ad6b102bd6160a0774bc0955a72e05fbf054a63
jwsoh08 <sohjunwen.govtech@gmail.com>
2024-02-28 23:27:42 +0800

    Move some constants to settings.py for cleaner code

---
+++ settings.py
@@ -0,0 +1,63 @@
+# constants
+
+PRI_LEVELS = [
+    "Primary 1",
+    "Primary 2",
+    "Primary 3",
+    "Primary 4",
+    "Primary 5",
+    "Primary 6",
+]
+
+SEC_LEVELS = [
+    "Secondary 1",
+    "Secondary 2",
+    "Secondary 3",
+    "Secondary 4",
+    "Secondary 5",
+]
+
+JC_LEVELS = [
+    "Junior College 1",
+    "Junior College 2",
+    "Junior College 3",
+]
+
+EDUCATION_LEVELS = PRI_LEVELS + SEC_LEVELS + JC_LEVELS
+
+SUBJECTS_LIST = [
+    "English Language",
+    "Chinese",
+    "Malay",
+    "Tamil",
+    "Mathematics",
+    "Science",
+    "Social Studies",
+    "Physical Education (PE)",
+    "Art",
+    "Music",
+    "Character and Citizenship Education (CCE)",
+    "Design and Technology",
+    "Food and Consumer Education (FCE)",
+    "Computer Studies",
+    "Additional Mathematics (Secondary)",
+    "Literature (Secondary)",
+    "History (Secondary)",
+    "Geography (Secondary)",
+    "Physics (Secondary)",
+    "Chemistry (Secondary)",
+    "Biology (Secondary)",
+    "Economics (JC)",
+    "Accounting (JC)",
+    "General Paper (JC)",
+    "Mathematics (JC)",
+    "Further Mathematics (JC)",
+    "Physics (JC)",
+    "Chemistry (JC)",
+    "Biology (JC)",
+    "History (JC)",
+    "Geography (JC)",
+    "Art (JC)",
+    "Music (JC)",
+    "Theatre Studies and Drama (JC)",
+]
